40ed5d310c9f200a6b28fdf3469c21527270964a
github username <github email address>
2019-01-13 16:06:53 -0500

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,24 @@
+class Artist
+
+  @@all = []
+
+  attr_accessor :name
+
+  def initialize(name = "")
+    @name = name
+
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def songs
+    Song.all.select do |song|
+      song.artist == self
+    end
+  end
+
+
+end
