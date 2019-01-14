dbd93cb457be232a4bb8464e68fdd17e99cb91cd
powell956 <powell956@gmail.com>
2019-01-13 19:14:08 -0500

    Done.

---
+++ lib/author.rb
@@ -0,0 +1,17 @@
+class Author
+
+  attr_accessor :name#, :genre
+
+  # @@all = []
+  #
+  # def initialize(name)
+  #   @name = name
+  #
+  #   @@all << self
+  # end
+  #
+  # def self.all
+  #   @@all
+  # end
+
+end
